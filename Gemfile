56d476fe93df168d21070fe3b31db70275bcedfa
harshit7308 <harshitnasith9lite@gmail.com>
2021-03-21 18:35:04 +0530

    add activeadmin

--- Gemfile
+++ Gemfile
@@ -17,8 +17,11 @@ gem 'sass-rails', '>= 6'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 
+gem 'activeadmin'
+
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
---
+++ app/admin/shoes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Shoe do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :category_id, :brand_id, :color_id, :size, :price, :discount_type, :discount, :suitable, :material
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :category_id, :brand_id, :color_id, :size, :price, :discount_type, :discount, :suitable, :material]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end

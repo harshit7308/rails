2058732d20f9ee634c5a5f5b9bc2d98c59c69b74
harshit7308 <harshitnasith9lite@gmail.com>
2021-03-22 17:12:28 +0530

    Admin shoe changes

--- app/admin/shoes.rb
+++ app/admin/shoes.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Shoe do
-  
+
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description, :category_id, :brand_id, :color_id, :size, :price, :discount_type, :discount, :suitable, :material
+  permit_params :name, :description, :category_id, :brand_id, :color_id, :size, :price, :discount_type, :discount, :suitable, :material
   #
   # or
   #
